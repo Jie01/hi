d96d6fbed4deb26bca75a739b37a729f250c089a
Jie01 <aukitty0504@gmail.com>
2021-01-23 22:48:29 +0800

    second commit

--- 02.py
+++ 02.py
@@ -15,7 +15,7 @@
 #teach "https://www.instructables.com/id/Python-coding-for-Minecraft/"
 
 # from mine import *
-import time
+# import time
 
 # mc = Minecraft()
 # while True: 
@@ -24,20 +24,47 @@ import time
 #     time.sleep(1)
 
 
-passw = input("set your password: ")
-print("your password is set")
-times = 3
-while times > 0:
-    repass = input("please enter your password: ")
-    if repass == passw:
-        print("login successful")
-        times=3
-        break
-    else:
-        times-=1
-        if times != 0:
-            print("password is wrong, please enter again")
-            print("you remain ",times, " times")
-        else:
-            print("account is locked, you can't enter anymore")
-    time.sleep(0.5)
+
+# passw = input("set your password: ")
+# print("your password is set")
+# times = 3
+# while times > 0:
+#     repass = input("please enter your password: ")
+#     if repass == passw:
+#         print("login successful")
+#         times=3
+#         break
+#     else:
+#         times-=1
+#         if times != 0:
+#             print("password is wrong, please enter again")
+#             print("you remain ",times, " times")
+#         else:
+#             print("account is locked, you can't enter anymore")
+#     time.sleep(0.5)
+
+
+
+# import random as ran
+# s = int(input("enter the start of range: "))
+# e = int(input("enter the end of range: "))
+# r = ran.randint(s,e)
+# times = 0
+# while True:
+#     times+=1
+#     uans = int(input ("guess the number : "))
+#     if uans == r:
+#         print("you are right")
+#         print("you guessed ", times , " times")
+#         break
+#     elif uans < r:
+#         print("bigger than you ans")
+#     elif uans > r:
+#         print("smaller than your ans")
+    
+# a = ['i' ,"love" , 'you', "Winnie"]
+# n = 'abcd'
+# for x in a*2:
+#     print(x)
+# for x in n:
+#     print(x)
