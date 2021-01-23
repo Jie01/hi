0f580e3a65db69aa56223f1d75830fe2f0b33b9b
Jie01 <aukitty0504@gmail.com>
2021-01-23 22:47:14 +0800

    new file

--- 01.py
+++ 01.py
@@ -1,3 +1,24 @@
-height = input("enter your height:")
-weight = input("enter your weight:")
+# height = float(input("enter your height(m):"))
+# weight = float(input("enter your weight(kg):"))
 
+# def BMI():
+#     value = weight/(height*height)
+#     print(value)
+# BMI()
+
+data = []
+count = 0
+with open('reviews.txt' , "r") as f:
+    for line in f:
+        data.append(line)
+        count+=1
+        # if count % 1000 == 0: #1000 的倍數才print
+        #     print(len(data))
+
+
+# 資料平均長度
+# lsum = 0
+# for d in data:
+#     lsum += len(d)
+# lsum =  lsum/len(data)
+# print(lsum)
